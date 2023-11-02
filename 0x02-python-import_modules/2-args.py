3c10b822818472f5db482e7b6112a7e7668cd4a7
TommyM1 <popoolaajayio@gmail.com>
2023-11-02 05:38:40 -0700

    Task 2 added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+    
     argLen = len(sys.argv)
     if argLen == 1:
         print("{} arguments.".format(argLen - 1))
@@ -8,5 +10,5 @@ if __name__ == "__main__":
         print("{} argument:".format(argLen - 1))
     else:
         print("{} arguments:".format(argLen - 1))
-        for i in range(1, argLen):
+    for i in range(1, argLen):
         print("{}: {}".format(i, sys.argv[i]))`

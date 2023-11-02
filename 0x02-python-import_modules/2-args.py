3343e959e6998005eb6719666fbd83af6439eff2
TommyM1 <popoolaajayio@gmail.com>
2023-11-02 05:53:54 -0700

    Task 2 added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} argument:".format(count))
-        for i in range(count):
+
+    for i in range(count):
             print("{}: {}".format(i + 1, sys.argv[i + 1]))
